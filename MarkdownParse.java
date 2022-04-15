64f98394e0142198ef7a3b44eb1288b75ff3b894
ni-chiu <nichiu@ucsd.edu>
2022-04-15 15:31:13 -0700

    Lab 2 Part 4

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,12 +11,16 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
+        int openBracket = 0;
         while(currentIndex < markdown.length()) {
-            System.out.println(markdown.length());
-            int openBracket = markdown.indexOf("[", currentIndex);
+            //System.out.println(markdown.length());
+            openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
+            if (openBracket < 0) {
+                break;
+            }
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
             System.out.println("Brackets: " + openBracket + ", " + closeBracket);
